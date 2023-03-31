39c567d29f6fc3a965e7c9ac56b5e84295572b6f
Alan Pereyro <alanpereyro0@gmail.com>
2023-03-30 21:07:26 -0400

    cleaned up javascript

--- script.js
+++ script.js
@@ -2,26 +2,38 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password input
 function writePassword() {
-const uppercase = [ "ABCDEFGHIJKLMNOPQRSTUVWXYZ" ];
-const lowercase = ["abcdefghijklmnopqrstuvwxyz" ];
-const numbers = ["0123456789"];
-const specials = ["!@#$%^&*()_+-=" ];
-var password = "";
-var passwordLength = 0;
-while (passwordLength < 8 || passwordLength > 128) {
-  passwordLength = parseInt(prompt("Please enter the desired password length between 8 and 128 characters:"));
-}
-var inputisValid = false;
-while (!inputisValid){
-var useLowercase = confirm("Do you want to include lowercase letters in your password?");
-  var useUppercase = confirm("Do you want to include uppercase letters in your password?");
-  var useNumbers = confirm("Do you want to include numbers in your password?");
-  var useSpecials = confirm("Do you want to include special characters in your password?");
-  inputisValid = useLowercase || useUppercase || useNumbers || useSpecials;
-  if (!inputisValid) {
-    alert("You must chose at least one character type for your password.");
+  const uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+  const lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+  const numbers = ["0123456789"];
+  const specials = ["!@#$%^&*()_+-="];
+  var password = "";
+  var passwordLength = 0;
+  if (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = parseInt(
+      prompt(
+        "Please enter the desired password length between 8 and 128 characters:"
+      )
+    );
   }
-}
+  var inputisValid = false;
+  if (!inputisValid) {
+    var useLowercase = confirm(
+      "Do you want to include lowercase letters in your password?"
+    );
+    var useUppercase = confirm(
+      "Do you want to include uppercase letters in your password?"
+    );
+    var useNumbers = confirm(
+      "Do you want to include numbers in your password?"
+    );
+    var useSpecials = confirm(
+      "Do you want to include special characters in your password?"
+    );
+    inputisValid = useLowercase || useUppercase || useNumbers || useSpecials;
+    if (!inputisValid) {
+      alert("You must chose at least one character type for your password.");
+    }
+  }
   var charset = "";
   if (useLowercase) {
     charset += lowercase;
